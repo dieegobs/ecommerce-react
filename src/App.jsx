b59c37a21e846a3e0f8d31179c2a89cc9c4cf001
dieegobs <diegohevant@gmail.com>
2023-03-03 23:56:41 -0300

    Criando nav projeto

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ const App = () => {
     <>
      <header>
         <img src={LogoSneakes} alt="" />
+        <nav>
+            <a href="#">Collections</a>
+            <a href="#">Men</a>
+            <a href="#">Women</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+
+        </nav>
      </header>
     </>
   );
